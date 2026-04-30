25767d2a7b2526724904012ac6d25f070de60201
Kaa1iru <kyle120203@gmail.com>
2026-04-30 21:16:05 +0800

    change port

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ import fishRoutes from "./routes/fish.routes.js";
 dotenv.config({ path: ".env" });
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
