7ad5cbf64cd2f0a2a58bee2ee63067189fa50d59
Kaa1iru <kyle120203@gmail.com>
2026-05-04 22:09:01 +0800

    testing

--- backend/src/python/app.py
+++ backend/src/python/app.py
@@ -2,17 +2,18 @@ from fastapi import FastAPI, File, UploadFile
 import numpy as np
 import cv2 as cv
 import joblib
+from typing import Optional
 
 app = FastAPI()
 
 model = joblib.load("quality_model.pkl")
 
 @app.post("/process_fish")
-async def image_analysis(file: UploadFile = File(...),
+async def image_analysis(fish_image: UploadFile = File(...),
                          gill_image: Optional[UploadFile] = File(None),
                          eye_image: Optional[UploadFile] = File(None)):
     
-    data = await file.read()
+    data = await fish_image.read()
 
     image_bfr = np.frombuffer(data, np.uint8)
     img = cv.imdecode(image_bfr, cv.IMREAD_COLOR)
@@ -33,14 +34,13 @@ async def image_analysis(file: UploadFile = File(...),
         "has_fish": True,
         "species": "Tilapia",
         "ml_score": round(ml_score, 2),
-        "rule_score": round(rule_score, 2),
-        "final_score": round(final_score, 2),
-        "quality": quality,
         "features": {
             "eye_score": 0.70,
             "gill_score": 0.65,
             "body_score": 0.72,
-        }
+        },
+        "final_score": round(final_score, 2),
+        "quality": quality,
     }
 
 # placeholder route for predict.py
